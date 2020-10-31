208ce731c437ef6c691163dc7be4c225257db1fd
SamatrixSun <sunyue163@mails.ucas.ac.cn>
2020-10-31 15:59:20 +0800

    Try to update

--- mybot/lita_config.rb
+++ mybot/lita_config.rb
@@ -21,6 +21,7 @@ Lita.configure do |config|
   if ENV['RACK_ENV']=='production'
     config.robot.adapter=:slack
     config.redis[:url]=ENV.fetch('REDIS_URL')
+    config.adapters.slack.token = "xoxb-1452819079319-1480191901169-N98zoQastmUsi52dRw01jK03"
   else
     config.robot.adapter = :shell
   end
@@ -30,10 +31,6 @@ Lita.configure do |config|
   ## Example: Set options for the chosen adapter.
   # config.adapter.username = "myname"
   # config.adapter.password = "secret"
-  Lita.configure do |config|
-    config.robot.adapter = :slack
-    config.adapters.slack.token = "xoxb-1452819079319-1480191901169-N98zoQastmUsi52dRw01jK03"
-  end
 
   ## Example: Set options for the Redis connection.
   # config.redis.host = "127.0.0.1"
